2cf6feebfd6eeac7877759f52b4aa22554f9a1cb
bugmaker <67356355+zyh5119232@users.noreply.github.com>
2022-09-20 00:12:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,5 +50,7 @@ wm = WeChatMessage(client)
 #wea, temperature = get_weather()
 #data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 #res = wm.send_template(user_id, template_id, data)
-my = wm.send_template(user_id, template_id)
+
+data = {}
+my = wm.send_template(user_id, template_id, data)
 print(my)
