31532862e5c0a81e5b8432fe8873132ce7d9b00b
bugmaker <67356355+zyh5119232@users.noreply.github.com>
2022-09-20 00:08:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,27 +18,27 @@ user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+#def get_weather():
+#  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+#  res = requests.get(url).json()
+#  weather = res['data']['list'][0]
+#  return weather['weather'], math.floor(weather['temp'])
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+#def get_count():
+#  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+#  return delta.days
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+#def get_birthday():
+#  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+#  if next < datetime.now():
+#    next = next.replace(year=next.year + 1)
+#  return (next - today).days
 
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+#def get_words():
+#  words = requests.get("https://api.shadiao.pro/chp")
+#  if words.status_code != 200:
+#    return get_words()
+#  return words.json()['data']['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
@@ -47,8 +47,8 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
+#wea, temperature = get_weather()
+#data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+#res = wm.send_template(user_id, template_id, data)
 my = wm.send_template(user_id, template_id)
 print(my)
