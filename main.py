6b40ead912fa326b756a1dda848d61e3c627e4aa
bugmaker <67356355+zyh5119232@users.noreply.github.com>
2022-09-20 00:21:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
-# template_id = os.environ["TEMPLATE_ID"]
+template_id = os.environ["TEMPLATE_ID"]
 
 
 #def get_weather():
